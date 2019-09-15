7e838a255ba103b83eb791c333c619491e868f75
Victor Johnson <vjohnson22@gmail.com>
2019-09-15 11:49:56 -0400

    got week label working

--- madden/src/TrendsContainer/TrendsContainer.js
+++ madden/src/TrendsContainer/TrendsContainer.js
@@ -12,13 +12,24 @@ import VsLineChart from '../VsLineChart/VsLineChart'
 
 class TrendsContainer extends React.Component{
     render(){
+        
         let games
         let weekLabel 
-        if (this.props.game === undefined){
+        if (this.props.game === undefined ){
             games = []
+            
         }else if (this.props.versus === "All"){
             games = this.props.game.filter( game => {
                 if (game.lost === `https://maddenstats.herokuapp.com/owners/${this.props.id}` || game.won === `https://maddenstats.herokuapp.com/owners/${this.props.id}`){
+                    return true
+                } 
+            })
+            weekLabel = games.map( game => {
+                return `Week ${game.week}, ${game.season}`
+            })
+        }else if (this.props.versus !== "All" ){
+            games = this.props.game.filter( game => {
+                if ((game.lost === `https://maddenstats.herokuapp.com/owners/${this.props.id}` && game.won === `https://maddenstats.herokuapp.com/owners/${this.props.versusCode}`)||(game.lost === `https://maddenstats.herokuapp.com/owners/${this.props.versusCode}` && game.won === `https://maddenstats.herokuapp.com/owners/${this.props.id}`)){
                     return true
                 } 
             })
