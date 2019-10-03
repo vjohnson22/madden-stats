b0b53444f448ab94d0bf43e505bb43fa66b274c8
Victor Johnson <vjohnson22@gmail.com>
2019-10-03 14:28:13 -0400

    stats and formatting for player stats updated

--- madden/src/PlayerStats/PlayerStats.js
+++ madden/src/PlayerStats/PlayerStats.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import './PlayerStats.css'
 
 class PlayerStats extends React.Component{
     constructor() {
@@ -25,7 +26,10 @@ class PlayerStats extends React.Component{
             sacks_avg:"",
             interceptions_avg:"",
             defensive_tds_avg:"",
-            games_played:""
+            games_played:"",
+            pass_int_avg:"",
+            forced_fumbles_avg: "",
+            pass_defended_avg:"",
         }
     }
     componentDidMount() {
@@ -51,6 +55,7 @@ class PlayerStats extends React.Component{
         let times_sacked = 0
         let pass_complete = 0
         let pass_attempt  = 0
+        let pass_int = 0
         let rush_yards = 0
         let rush_tds = 0
         let fumbled = 0
@@ -63,12 +68,15 @@ class PlayerStats extends React.Component{
         let sacks = 0
         let interceptions = 0
         let defensive_tds = 0
+        let forced_fumbles = 0 
+        let pass_defended = 0
 
         let pass_yards_avg = 0
         let pass_td_avg = 0
         let times_sacked_avg = 0
         let pass_complete_avg = 0
         let pass_attempt_avg = 0
+        let pass_int_avg
         let rush_yards_avg = 0
         let rush_tds_avg = 0
         let fumbled_avg = 0
@@ -81,6 +89,8 @@ class PlayerStats extends React.Component{
         let sacks_avg = 0
         let interceptions_avg = 0
         let defensive_tds_avg = 0
+        let forced_fumbles_avg = 0
+        let pass_defended_avg = 0
 
         let games_played = 0
 
@@ -91,6 +101,7 @@ class PlayerStats extends React.Component{
                 times_sacked += game.times_sacked 
                 pass_complete += game.pass_complete
                 pass_attempt  += game.pass_attempt
+                pass_int += game.pass_int
                 rush_yards += game.rush_yards
                 rush_tds += game.rush_tds
                 fumbled += game.fumbled
@@ -103,6 +114,8 @@ class PlayerStats extends React.Component{
                 sacks += game.sacks
                 interceptions += game.interceptions
                 defensive_tds += game.defensive_tds
+                forced_fumbles += game.forced_fumbles
+                pass_defended += game.pass_defended
             })
 
             pass_yards_avg = pass_yards / this.state.playerStats.length
@@ -110,6 +123,7 @@ class PlayerStats extends React.Component{
             times_sacked_avg = times_sacked / this.state.playerStats.length
             pass_complete_avg = pass_complete / this.state.playerStats.length
             pass_attempt_avg = pass_attempt/ this.state.playerStats.length
+            pass_int_avg = pass_int / this.state.playerStats.length
             rush_yards_avg = rush_yards / this.state.playerStats.length
             rush_tds_avg = rush_tds / this.state.playerStats.length
             fumbled_avg = fumbled / this.state.playerStats.length
@@ -123,6 +137,8 @@ class PlayerStats extends React.Component{
             interceptions_avg = interceptions / this.state.playerStats.length
             defensive_tds_avg = defensive_tds / this.state.playerStats.length
             games_played = this.state.playerStats.length
+            forced_fumbles_avg = forced_fumbles / this.state.playerStats.length
+            pass_defended_avg = pass_defended / this.state.playerStats.length
 
             if(this.state.pass_td_avg === ""){
                 this.setState({
@@ -143,7 +159,10 @@ class PlayerStats extends React.Component{
                     sacks_avg: sacks_avg,
                     interceptions_avg: interceptions_avg,
                     defensive_tds_avg: defensive_tds_avg,
-                    games_played:games_played
+                    games_played:games_played,
+                    pass_int_avg: pass_int_avg,
+                    forced_fumbles_avg: forced_fumbles_avg,
+                    pass_defended_avg: pass_defended_avg
                 })
             }
 
@@ -162,10 +181,11 @@ class PlayerStats extends React.Component{
                         <h2>User Games Played: {this.state.games_played}</h2>
                         <h2>Pass Yards: {this.state.pass_yards_avg}</h2>
                         <h2>Pass Tds: {this.state.pass_td_avg}</h2>
-                        <h2>Interceptions: {this.state.interceptions_avg} </h2>
+                        <h2>Interceptions Thrown: {this.state.pass_int_avg} </h2>
                         <h2>Times Sacked: {this.state.times_sacked_avg}</h2>
                         <h2>Passes Completed: {this.state.pass_complete_avg}</h2>
                         <h2>Passes Attempted:{this.state.pass_attempt_avg}</h2>
+                        <h2>Completion%: {((this.state.pass_complete_avg / this.state.pass_attempt_avg)*100).toFixed(0)} </h2>
                         <h2>Rush Yards: {this.state.rush_yards_avg}</h2>
                         <h2>Rushing Tds: {this.state.rush_tds_avg}</h2>
                         <h2>Fumbles: {this.state.fumbled_avg}</h2>
@@ -192,16 +212,20 @@ class PlayerStats extends React.Component{
                         <h2>Tackles: {this.state.tackles_avg}</h2>
                         <h2>Tackles for Loss: {this.state.tfl_avg}</h2>
                         <h2>Sacks: {this.state.sacks_avg}</h2>
+                        <h2>Forced Fumbles: {this.state.forced_fumbles_avg}</h2>
                         <h2>Interceptions: {this.state.interceptions_avg}</h2>
+                        <h2>Passes Defended: {this.state.pass_defended_avg}</h2>
                         <h2>Defensive Tds: {this.state.defensive_tds_avg}</h2>
                     </div>        
                 )
             }
         })
         return(
-            <div>
+            <div className= 'playerStatsGrid'>
+                <div>
                 <h1>User Game Averages</h1>
                 {table}
+                </div>
             </div>
         )
     }
